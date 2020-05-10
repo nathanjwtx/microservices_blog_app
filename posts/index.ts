0ec0e6a149eddebc316ebc340e5d37ee55c4d2f2
nathanjwtx <nathanjwtx@nathanjwtx.io>
2020-05-10 13:20:40 -0500

    added event bus code and logic

--- posts/index.ts
+++ posts/index.ts
@@ -2,26 +2,49 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const posts = {};
+type Post = {
+	id: string,
+	title: string
+};
+
+let posts:any = {};
 
 app.get('/posts', (_req: any, res: { send: (arg0: {}) => void; }) => {
 	res.send(posts);
 });
 
-app.post('/posts', (req: { body: { title: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; send: { (arg0: any): void; new(): any; }; }; }) => {
+app.post('/posts', async (req: { body: { title: string; }; }, res: { status: (arg0: number) => { (): any; new(): any; send: { (arg0: any): void; new(): any; }; }; }) => {
 	const id = randomBytes(4).toString('hex');
-	const { title } = req.body;
 
-	posts[id] = {
-		id, title
+	const post = {} as Post;
+	post.id = id;
+	post.title = req.body.title;
+	posts[id] = post;
+
+	type Event = {
+		type: string,
+		data: Post
 	};
 
+	let event = {} as Event;
+	event.type = 'PostCreated';
+	event.data = post;
+
+	await axios.post('http://localhost:4005/events', event);
+
 	res.status(201).send(posts[id]);
+});
+
+app.post('/events', (req, res) => {
+	console.log('received event', req.body.type);
+
+	res.send({});
 });
 
 app.listen(4000, () => {
