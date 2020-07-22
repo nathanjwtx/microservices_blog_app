61f6bcfbfe0546feb5b4bfbd0236e7a96b391e57
Nathan <nathanjwtx@nathanjwtx.io>
2020-07-22 08:06:54 -0500

    created dockerfiles for all services

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,12 +5,17 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+// this will hold all events so that a stopped service can pick up where it
+// left off if it has stopped and been restarted
+const events = []
+
 app.post('/events', (req, res) => {
 	const event = req.body;
 
+	events.push(event)
+
 	// event-bus sends out the event to all microservices upon receipt
 	// each microservice may or may not do something with it
-
 	axios.post('http://localhost:4000/events', event);
 	axios.post('http://localhost:4001/events', event);
 	axios.post('http://localhost:4002/events', event);
@@ -18,6 +23,11 @@ app.post('/events', (req, res) => {
 
 	res.send({ status: 'OK' });
 });
+
+app.get('/events', (req, res) => {
+	console.log('events called for');
+	res.send(events)
+})
 
 app.listen(4005, () => {
 	console.log('Event-bus listening on port 4005');
